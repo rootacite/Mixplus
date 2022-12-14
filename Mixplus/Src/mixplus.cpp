33aea71d83166556e88fb8a1a63883ccbb76ea96
世元 李 <1498045907@qq.com>
2022-12-14 16:06:33 +0800

    2022.12.14

--- Mixplus/Src/mixplus.cpp
+++ Mixplus/Src/mixplus.cpp
@@ -7,12 +7,15 @@
 
 using namespace std;
 
+
+Serial* Serial1 = NULL;
+
 void setup()
 {
-
+    Serial1 = new Serial(&huart1);
 }
 
-void loop()
-{
-
+void loop() {
+    string rev = Serial1->readline();
+    Serial1->writeline("Received:" + rev);
 }
